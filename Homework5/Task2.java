7686bb0a77e872f7f03a4fb094aa0db57d067ada
Dmitriy <Wonderful_days@mail.ru>
2023-03-25 09:00:26 +0300

    Complete second task

---
+++ Homework5/Task2.java
@@ -0,0 +1,73 @@
+package Homework5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * Пусть дан список сотрудников:
+Иван Иванов
+Светлана Петрова
+Кристина Белова
+Анна Мусина
+Анна Крутова
+Иван Юрин
+Петр Лыков
+Павел Чернов
+Петр Чернышов
+Мария Федорова
+Марина Светлова
+Мария Савина
+Мария Рыкова
+Марина Лугова
+Анна Владимирова
+Иван Мечников
+Петр Петин
+Иван Ежов
+
+Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
+Отсортировать по убыванию популярности Имени.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        List<String> employees = Arrays.asList(
+                "Иван Иванов",
+                "Светлана Петрова",
+                "Кристина Белова",
+                "Анна Мусина",
+                "Анна Крутова",
+                "Иван Юрин",
+                "Петр Лыков",
+                "Павел Чернов",
+                "Петр Чернышов",
+                "Мария Федорова",
+                "Марина Светлова",
+                "Мария Савина",
+                "Мария Рыкова",
+                "Марина Лугова",
+                "Анна Владимирова",
+                "Иван Мечников",
+                "Петр Петин",
+                "Иван Ежов"
+        );
+
+        Map<String, Integer> count = new HashMap<>();
+
+        for (String employee : employees) {
+            String name = employee.split(" ")[0];
+            count.put(name, count.getOrDefault(name, 0) + 1);
+        }
+
+        List<Map.Entry<String, Integer>> sortedCount = new ArrayList<>(count.entrySet());
+        sortedCount.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+
+        for (Map.Entry<String, Integer> sortedList : sortedCount) {
+            if (sortedList.getValue() > 1) {
+                System.out.println(sortedList.getKey() + ": " + sortedList.getValue());
+            }
+        }
+    }
+}
